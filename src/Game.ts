a68b7a392c616753a24152ce529f211fcb0ab4fa
Jens CN <jens.sydaker@gmail.com>
2024-09-05 14:32:59 +0200

    Fortsatt på spellogiken

--- src/Game.ts
+++ src/Game.ts
@@ -1,3 +1,4 @@
+import readlineSync from "readline-sync";
 import { Board } from "./Board";
 import { Player } from "./Player";
 import { validateInput } from "./Utilities";
@@ -23,5 +24,50 @@ export class Game {
       new Player("Player 1", "X"),
       this.isVsBot ? new Player("Bot", "O") : new Player("Player 2", "O"),
     ];
+  }
+
+  private chooseGameMode(): void {
+    const choice = readlineSync.question(
+      "Choose gamemode: 1 for PvP, 2 for PvB: "
+    );
+    this.isVsBot = choice === "2";
+  }
+
+  public start(): void {
+    let isGameOver = false;
+
+    while (!isGameOver) {
+      this.board.display();
+      const currentPlayer = this.players[this.currentPlayerIndex];
+      console.log(`${currentPlayer.name}'s turn (${currentPlayer.symbol})`);
+
+      let column: number;
+
+      if (this.isVsBot && currentPlayer.name === "Bot") {
+        column = getRandomColumn(this.board);
+        console.log(`The bot chooses column ${column + 1}`);
+      } else {
+        column = this.getPlayerMove();
+      }
+
+      const success = this.board.placeToken(column, currentPlayer.symbol);
+
+      if (!success) {
+        console.log("Column is full. Try another one.");
+        continue;
+      }
+
+      if (this.board.checkWin(currentPlayer.symbol)) {
+        this.board.display();
+        console.log(`${currentPlayer.name} Wins!`);
+        isGameOver = true;
+      } else if (this.board.isFull()) {
+        this.board.display();
+        console.log("It's a draw!");
+        isGameOver = true;
+      } else {
+        this.currentPlayerIndex = 1 - this.currentPlayerIndex;
+      }
+    }
   }
 }
