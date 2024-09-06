91036625ed1d285ce278b2c4daa7c1714826860f
Jens CN <jens.sydaker@gmail.com>
2024-09-06 19:52:52 +0200

    infört så brickorna hamnar längst ner på brädet

--- src/Board.ts
+++ src/Board.ts
@@ -19,4 +19,20 @@ export class Board {
       " " + Array.from({ length: this.columns }, (_, i) => i + 1).join(" ")
     );
   }
+
+  //Loopar igenom kolumnerna för att lägga symbolen längst ner.
+  public placeToken(column: number, symbol: string): boolean {
+    for (let row = this.rows - 1; row >= 0; row--) {
+      //Kollar ifall det finns en ledig plats i kolumnen.
+      if (this.grid[row][column] === " ") {
+        this.grid[row][column] = symbol;
+        return true;
+      }
+    }
+    return false;
+  }
+
+  public canPlaceToken(column: number): boolean {
+    return this.grid[0][column] === " ";
+  }
 }
